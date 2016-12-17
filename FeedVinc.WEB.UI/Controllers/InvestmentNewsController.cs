4d7711a738f65f108102dd66ede685563e0a4bc6
mert.alptekin <mert.alptekin@polynomtech.com>
2016-12-17 01:40:26 -0800

    Feedvinc InvestedNews

--- FeedVinc.WEB.UI/Controllers/InvestmentNewsController.cs
+++ FeedVinc.WEB.UI/Controllers/InvestmentNewsController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using PagedList;
 using PagedList.Mvc;
+using FeedVinc.WEB.UI.Models.ViewModels.Project;
 
 namespace FeedVinc.WEB.UI.Controllers
 {
@@ -20,22 +21,31 @@ namespace FeedVinc.WEB.UI.Controllers
 
             var project = services.projectRepo.Where(x => x.IsInvested == true).ToList();
 
-            var projectIDs = project.Select(a => a.ID);
+            var projectIDs = project.Select(a => a.ID).ToList();
 
             var model = services.ideaShareRepo.Where(x => projectIDs.Contains(x.ProjectID)).Select(a => new InvestedNewsVM
             {
-                ProjectCode = project.FirstOrDefault(c=> c.ID==a.ProjectID).ProjectCode,
-                ProjectName = project.FirstOrDefault(c => c.ID == a.ProjectID).ProjectName,
-                ProjectSlug = project.FirstOrDefault(c => c.ID == a.ProjectID).ProjectSlugify,
-                PrettyDate = DateTimeService.GetPrettyDate(a.PostDate,LanguageService.getCurrentLanguage),
+
                 ShareCount = a.ShareCount,
-                SalesPitch = project.FirstOrDefault(c => c.ID == a.ProjectID).SalesPitch,
-                UserID = (long)a.OwnerID,
-                ShareID = a.ID
+                ShareID = a.ID,
+                ProjectID = a.ProjectID,
+                PrettyDate = DateTimeService.GetPrettyDate(a.PostDate,LanguageService.getCurrentLanguage)
 
             }).ToList();
 
-            model.ForEach(a => a.LikedCurrentUser = services.ideaShareLikeRepo.Any(x => x.UserID == a.UserID && x.IdeaShareID == a.ShareID));
+            model.ForEach(a => a.Project = services.projectRepo.Where(c => c.ID == a.ProjectID).Select(v => new ProjectVM
+            {
+                ProjectName = v.ProjectName,
+                ProjectSalesPitch = v.SalesPitch,
+                OwnerID = v.UserID,
+                ProjectSlugify = v.ProjectSlugify,
+                ProjectProfileLogo = v.ProjectProfileLogo,
+                ProjectCode = v.ProjectCode
+
+            }).FirstOrDefault());
+
+
+            model.ForEach(a => a.LikedCurrentUser = services.ideaShareLikeRepo.Any(x => x.UserID == a.Project.OwnerID && x.IdeaShareID == a.ShareID));
             model.ForEach(a => a.LikeCount = services.ideaShareLikeRepo.Count(c => c.IdeaShareID == a.ShareID));
             model.ForEach(a => a.CommentCount = services.ideaShareCommentRepo.Count(c => c.IdeaShareID == a.ShareID));
 
--- FeedVinc.WEB.UI/Models/ViewModels/Invested/InvestedNewsVM.cs
+++ FeedVinc.WEB.UI/Models/ViewModels/Invested/InvestedNewsVM.cs
@@ -7,21 +7,15 @@ namespace FeedVinc.WEB.UI.Models.ViewModels.Invested
 {
     public class InvestedNewsVM
     {
-        public string ProjectName { get; set; }
-        public string ProjectSlug { get; set; }
-        public string ProjectCode { get; set; }
-        public string SalesPitch { get; set; }
-        public string PrettyDate { get; set; }
+        public Project.ProjectVM Project { get; set; }
         public long LikeCount { get; set; }
         public long ShareCount { get; set; }
         public long CommentCount { get; set; }
-        public string ProfilePhoto { get; set; }
+        public long ProjectID { get; set; }
+        public string PrettyDate { get; set; }
 
         public long ShareID { get; set; }
         public bool LikedCurrentUser { get; set; }
-        public long UserID { get; set; }
-
-
 
     }
 }
